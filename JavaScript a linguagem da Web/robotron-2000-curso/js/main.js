8609121ce2167faa697b68b5ba5004dec36aa1ae
Ruan Narici <ruannarici@hotmail.com>
2023-06-14 00:07:38 -0300

    Alura-Javascript-DOM

--- JavaScript a linguagem da Web/robotron-2000-curso/js/main.js
+++ JavaScript a linguagem da Web/robotron-2000-curso/js/main.js
@@ -1,5 +1,40 @@
 const controleAjuste = document.querySelectorAll('[data-controle]');
-;
+const estatisticas = document.querySelectorAll('[data-estatistica]');
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
 
 controleAjuste.forEach(acao => {
     acao.addEventListener('click', (evento) => {
@@ -7,6 +42,7 @@ controleAjuste.forEach(acao => {
         // console.log(evento.target.parentNode);
         //console.log(evento.target.dataset.controle;)
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        atualizarValores(evento.target.dataset.peca, evento.target.dataset.controle);
     })
 })
 
@@ -18,3 +54,16 @@ manipulaDados = (operacao, controle) => {
         controleContador.value = parseInt(controleContador.value) +1;
     }
 }
+
+atualizarValores = (peca, operacao) => {
+
+    estatisticas.forEach(elemento => {
+        if (operacao === '-') {
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+        } else {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+        }        
+    })
+}
+
+
