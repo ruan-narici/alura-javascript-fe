6b03d918d066dadaeb2e7449a23cd705e9c0ff16
Ruan Narici <ruannarici@hotmail.com>
2023-06-22 18:27:07 -0300

    Alura-Javascript-REQUISICOES

--- Requisições e formulários/aluraplay-requisicoes-curso/js/mostrarVideo.js
+++ Requisições e formulários/aluraplay-requisicoes-curso/js/mostrarVideo.js
@@ -2,25 +2,31 @@ import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector('[data-lista');
 
-function constroiCard() {
+function constroiCard(titulo, descricao, url, imagem) {
     let video = document.createElement('li');
     video.className = 'video__item';
     video.innerHTML = `
-        <iframe width="100%" height="72%" src="https://www.youtube.com/embed/pA-EgOaF23I"
+        <iframe width="100%" height="72%" src="${url}"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
         </iframe>
         <div class="descricao-video">
-            <img src="./img/logo.png" alt="logo canal alura">
-            <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
-            <p>236 mil visualizações</p>
+            <img src="${imagem}" alt="logo canal alura">
+            <h3>${titulo}</h3>
+            <p>${descricao}</p>
         </div>
     `;
     return video;
 }
 
 async function listaVideo() {
-    let lista = await conectaApi.capturarApi();
-    console.log(lista)
+    let listaAPI = await conectaApi.capturarApi();
+    listaAPI.forEach(elemento => {
+        lista.appendChild(
+            constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)
+        );
+    })
 }
+
+listaVideo();
